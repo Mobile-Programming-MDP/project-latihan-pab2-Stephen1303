a54eee0b28b53b6854c5cb7bbfe4f41697d920f3
Stephen1303 <wndystvn13@mhs.mdp.ac.id>
2025-02-28 20:50:39 +0700

    Create Favorite & Search Screen
    Create Favorite Screen, Search Screen
    
    Update Main

--- aplikasi_pilem/lib/main.dart
+++ aplikasi_pilem/lib/main.dart
@@ -20,3 +20,48 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+  @override
+  MainScreenState createState() => MainScreenState();
+}
+class MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const SearchScreen(),
+    const FavoriteScreen(),
+  ];
+  void _onItemTapped(int index) {
+    setState(() {
+    _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+    body: _screens[_selectedIndex],
+    bottomNavigationBar: BottomNavigationBar(
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
+      items: const <BottomNavigationBarItem>
+        [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorite',
+          ),
+        ],
+      ),
+    );
+  }
+}
