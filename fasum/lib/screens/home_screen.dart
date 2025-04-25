ad867b6f13744ad09e5397aac94c4197d30c7c59
Stephen1303 <wndystvn13@mhs.mdp.ac.id>
2025-04-25 20:33:13 +0700

    Add Post
    Create Add Post

--- fasum/lib/screens/home_screen.dart
+++ fasum/lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fasum/screens/add_post_screen.dart';
 import 'package:fasum/screens/sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -15,7 +18,7 @@ class HomeScreen extends StatelessWidget {
       return '${diff.inSeconds} secs ago';
     } else if (diff.inMinutes < 60) {
       return '${diff.inMinutes} mins ago';
-    } else if (diff.inHours < 60) {
+    } else if (diff.inHours < 24) {
       return '${diff.inHours} hrs ago';
     } else {
       return DateFormat('dd/MM/yyyy').format(dateTime);
@@ -48,8 +51,8 @@ class HomeScreen extends StatelessWidget {
       body: StreamBuilder(
         stream:
             FirebaseFirestore.instance
-                .collection("post")
-                //.orderBy("createdAt", descending: true)
+                .collection("posts")
+                .orderBy("createdAt", descending: true)
                 .snapshots(),
         builder: (context, snapshot) {
           if (!snapshot.hasData)
@@ -57,17 +60,16 @@ class HomeScreen extends StatelessWidget {
 
           final posts = snapshot.data!.docs;
           return ListView.builder(
-            itemCount:
-                posts.length, //menampilkan sebanyak data yang ada di posts
+            itemCount: posts.length,
             itemBuilder: (context, index) {
               final data = posts[index].data();
-              //final imageBase64 = data['image'] ?? '';
-              final description = data['description'] ?? '';
-              //final createAtStrs = data['createAt']; //kapan posting dibuat
-              final fullname = data['fullname'] ?? 'Anonim';
+              final imageBase64 = data['image'];
+              final description = data['description'];
+              final createdAtStr = data['createdAt'];
+              final fullName = data['fullName'] ?? 'Anonim';
 
-              //Parse ke DateTime
-              //final createAt = DateTime.parse(createAtStrs);
+              //parse ke DateTime
+              final createdAt = DateTime.parse(createdAtStr);
               return Card(
                 margin: const EdgeInsets.all(10),
                 shape: RoundedRectangleBorder(
@@ -76,17 +78,53 @@ class HomeScreen extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      fullname,
-                      style: const TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
+                    if (imageBase64 != null)
+                      ClipRRect(
+                        borderRadius: const BorderRadius.vertical(
+                          top: Radius.circular(10),
+                        ),
+                        child: Image.memory(
+                          base64Decode(imageBase64),
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                          height: 200,
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 6),
-                    Text(
-                      description ?? '',
-                      style: const TextStyle(fontSize: 16),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 10,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                formatTime(createdAt),
+                                style: const TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              Text(
+                                fullName,
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              const SizedBox(height: 6),
+                            ],
+                          ),
+                          const SizedBox(height: 6),
+                          Text(
+                            description ?? '',
+                            style: const TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
@@ -96,7 +134,11 @@ class HomeScreen extends StatelessWidget {
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.of(
+            context,
+          ).push(MaterialPageRoute(builder: (context) => AddPostScreen()));
+        },
         child: const Icon(Icons.add),
       ),
     );
